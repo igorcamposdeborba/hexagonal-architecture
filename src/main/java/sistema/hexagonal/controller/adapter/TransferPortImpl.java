29faf9c572e228b4be7dd5b7494a4c7264c70186
igorcamposdeborba <igor_campos5@hotmail.com>
2023-09-10 22:25:18 -0300

    Update TransferPortImpl.java

--- src/main/java/sistema/hexagonal/controller/adapter/TransferPortImpl.java
+++ src/main/java/sistema/hexagonal/controller/adapter/TransferPortImpl.java
@@ -3,6 +3,11 @@ package sistema.hexagonal.controller.adapter;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import sistema.hexagonal.controller.TransferPort;
 import sistema.hexagonal.domain.Account;
 import sistema.hexagonal.domain.exceptions.BusinessException;
@@ -10,9 +15,15 @@ import sistema.hexagonal.domain.exceptions.ExceptionHandler;
 import sistema.hexagonal.repositories.AccountRepository;
 import sistema.hexagonal.service.Transfer;
 
+@Named // Injeção de dependência (Bean de configuração) caso não coloque como static o método. Daí @Named vai instanciar automaticamente
 public class TransferPortImpl implements TransferPort {
 
 	private AccountRepository accountRepository;
+	
+	@Inject // Injeta dependência primeiro aqui
+	public TransferPortImpl(AccountRepository accountRepository) {
+		this.accountRepository = accountRepository;
+	}
 	
 	@Override
 	public Account getAccountById(Integer accountId) throws ExceptionHandler {
@@ -21,6 +32,7 @@ public class TransferPortImpl implements TransferPort {
 	}
 
 	@Override
+	@Transactional // Transação (Begin até o Commit) para poder fazer rollback (voltar ao estado anterior) se necessário
 	public void transfer(BigDecimal amount, Account debit, Account credit) throws ExceptionHandler, BusinessException {
 		Account originAccount = getAccountById(debit.getAccountId());
 		Account destinationAccount = getAccountById(credit.getAccountId());
@@ -32,8 +44,6 @@ public class TransferPortImpl implements TransferPort {
 		Transfer.initiateTransfer(amount, debit, credit); // atualizar java entity
 		originAccount.debit(amount); // atualizar banco de dados
 		destinationAccount.credit(amount);
-		
-		
 	}
 
 }
