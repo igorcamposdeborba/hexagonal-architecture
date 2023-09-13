b4ad325cc2fd0b89273b803cfb26647cfa2875ee
igorcamposdeborba <igor_campos5@hotmail.com>
2023-09-12 21:42:48 -0300

    endpoint test JUnit

--- src/test/java/account/adapter/controller/TransferPortTest.java
+++ src/test/java/account/adapter/controller/TransferPortTest.java
@@ -5,10 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 
 import javax.inject.Inject;
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -26,7 +23,8 @@ import account.hexagonal.entities.exceptions.ExceptionHandler;
 	3) Exception: unexists account in database
    
    Method transfer:
-	1) Exception: same account A and B
+	4) Happy path: transfer from account a to account B
+	5) Exception: same account A and B
 	Não vou testar a transferência aqui no Controller porque já foi testado no Service. Se testar duas vezes chamando ambas as classes sem mock, vai debitar e creditar duas vezes (isso seria uma falha do programador).
 */
 
@@ -40,18 +38,12 @@ public class TransferPortTest {
 	@Test
 	void getAccountById_ShouldReturnAccount() throws ExceptionHandler, BusinessException {
 		// Arrange
-		BigDecimal balance1 = new BigDecimal(100);
-		BigDecimal balance2 = new BigDecimal(500);
-		
 		Integer originAccountId = 1;
 		Account originAccount = new Account(1, new BigDecimal(100), "Igor");
 		
 		Integer destinityAccountId = 2;
 		Account destinityAccount = new Account(2, new BigDecimal(500), "Roberto");
-		
-		Integer unexistsAccountId = 30;
-		Integer nullAccountId = null;
-		
+
 		// Act
 		Account account1 = transferPort.getAccountById(originAccountId);
 		Account account2 = transferPort.getAccountById(destinityAccountId);
@@ -82,10 +74,9 @@ public class TransferPortTest {
 	}
 
 	@Test
-	void sameAccountAccountOriginAndDestiny_ShouldThrowsBusinessException_WhenIsCalledMethodTransfer() throws ExceptionHandler, BusinessException {
+	void transferMoneyBetweenAccountOriginAndDestiny_ShouldTransferMoney_WhenIsCalledMethodTransfer() throws ExceptionHandler, BusinessException {
 		// Arrange
-		BigDecimal amount1 = new BigDecimal(100);
-		BigDecimal amount2 = new BigDecimal(500);
+		BigDecimal amount1 = new BigDecimal(50);
 		
 		Integer originAccountId = 1;
 		Account originAccount = new Account(1, new BigDecimal(100), "Igor");
@@ -93,11 +84,24 @@ public class TransferPortTest {
 		Integer destinityAccountId = 2;
 		Account destinityAccount = new Account(2, new BigDecimal(500), "Roberto");
 		
-		Integer unexistsAccountId = 30;
-		Integer nullAccountId = null;
+		transferPort.transfer(amount1, originAccount, destinityAccount);
+		
+		// Assert
+		Assertions.assertEquals(new BigDecimal(50), transferPort.getAccountById(originAccountId).getBalance());
+		Assertions.assertEquals(new BigDecimal(550), transferPort.getAccountById(destinityAccountId).getBalance());
+	}
+	
+	@Test
+	void sameAccountAccountOriginAndDestiny_ShouldThrowsBusinessException_WhenIsCalledMethodTransfer() throws ExceptionHandler, BusinessException {
+		// Arrange
+		BigDecimal amount1 = new BigDecimal(100);
+
+		Account originAccount = new Account(1, new BigDecimal(100), "Igor");
 		
 		// Assert
 		BusinessException message = Assertions.assertThrows(BusinessException.class, () -> transferPort.transfer(amount1, originAccount, originAccount), "Revise os parametros e a logica de processamento");
 		assertEquals("Conta debito e credito devem ser diferentes", message.getMessage());
 	}
+	
+
 }
